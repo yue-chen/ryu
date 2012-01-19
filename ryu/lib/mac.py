554527d7dc19fe82a121d62cfe78e6cd377ddada
Isaku Yamahata <yamahata@valinux.co.jp>
2012-01-19 13:16:28 +0900

    lib/mac: add helper function/values
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/lib/mac.py
+++ ryu/lib/mac.py
@@ -14,6 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+dontcare = '\x00' * 6
+broadcast = '\xff' * 6
+
+
+def is_multicast(addr):
+    return bool(ord(addr[0]) & 0x01)
+
+
 def haddr_to_str(addr):
     return ''.join(['%02x:' % ord(char) for char in addr[0:6]])[:-1]
 
