404c016783d229fd5ffcf6bb52bb14a809e87552
Isaku Yamahata <yamahata@valinux.co.jp>
2012-01-20 16:11:13 +0900

    controller/dispatcher: introduce events on event queue itself
    This patch introduces event queue event(event on event queue).
    And track dispatcher children in order to register callback handler
    for all children of a given dispatcher.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/controller/dispatcher.py
+++ ryu/controller/dispatcher.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
-# Copyright (C) 2011 Isaku Yamahata <yamahata at valinux co jp>
+# Copyright (C) 2011, 2012 Isaku Yamahata <yamahata at valinux co jp>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,23 +18,40 @@ import weakref
 
 from gevent.queue import Queue
 
+from . import event
 
 LOG = logging.getLogger('ryu.controller.dispatcher')
 
 
 class EventQueue(object):
     event_queues = weakref.WeakSet()
+    ev_q_weakref = None
 
-    def __init__(self, name, dispatcher):
-        self.event_queues.add(self)
+    def _queue_q_ev(self, ev):
+        ev_q = self.ev_q_weakref
+        if ev_q is not None:
+            ev_q = ev_q()
+        if ev_q is not None:
+            ev_q.queue(ev)
 
+    def __init__(self, name, dispatcher, aux=None):
         self.name = name
-        self.dispatcher = dispatcher
+        self.dispatcher = dispatcher.clone()
         self.is_dispatching = False
         self.ev_q = Queue()
+        self.aux = aux  # for EventQueueCreate event
+
+        self.event_queues.add(self)
+        self._queue_q_ev(EventQueueCreate(self, True))
+
+    def __del__(self):
+        self._queue_q_ev(EventQueueCreate(self, False))
 
     def set_dispatcher(self, dispatcher):
-        self.dispatcher = dispatcher
+        old = self.dispatcher
+        new = dispatcher.clone()
+        self.dispatcher = new
+        self._queue_q_ev(EventDispatcherChange(self, old, new))
 
     def queue_raw(self, ev):
         self.ev_q.put(ev)
@@ -68,28 +85,49 @@ class EventDispatcher(object):
     event_dispatchers = weakref.WeakSet()
 
     def __init__(self, name):
-        self.event_dispatchers.add(self)
-
+        self.children = weakref.WeakSet()
         self.name = name
         self.events = {}
         self.all_handlers = []
 
+        self.event_dispatchers.add(self)
+
+    def clone(self):
+        cloned = EventDispatcher(self.name)
+        for ev_cls, h in self.events.items():
+            cloned.events[ev_cls] = copy.copy(h)
+        cloned.all_handlers = copy.copy(self.all_handlers)
+        self.children.add(cloned)
+        return cloned
+
+    def _foreach_children(self, call, *args, **kwargs):
+        for c in self.children:
+            call(c, *args, **kwargs)
+
     def register_all_handler(self, all_handler):
         self.all_handlers.append(all_handler)
+        self._foreach_children(EventDispatcher.register_all_handler,
+                               all_handler)
 
     def unregister_all_handler(self, all_handler):
+        self._foreach_children(EventDispatcher.unregister_all_handler,
+                               all_handler)
         del self.all_handlers[all_handler]
 
     def register_handler(self, ev_cls, handler):
         assert callable(handler)
         self.events.setdefault(ev_cls, [])
         self.events[ev_cls].append(handler)
+        self._foreach_children(EventDispatcher.register_handler,
+                               ev_cls, handler)
 
     def register_handlers(self, handlers):
         for ev_cls, h in handlers:
             self.register_handler(ev_cls, h)
 
     def unregister_handler(self, ev_cls, handler):
+        self._foreach_children(EventDispatcher.unregister_handler,
+                               ev_cls, handler)
         del self.events[ev_cls][handler]
 
     def register_static(self, ev_cls):
@@ -107,6 +145,9 @@ class EventDispatcher(object):
         self.dispatch(ev)
 
     def dispatch(self, ev):
+        # copy handler list because the list is not stable.
+        # event handler may block/switch thread execution
+        # and un/register other handlers.
         #LOG.debug('dispatch %s', ev)
 
         all_handlers = copy.copy(self.all_handlers)
@@ -119,15 +160,38 @@ class EventDispatcher(object):
             LOG.info('unhandled event %s', ev)
             return
 
-        # Is this necessary?
-        #
-        # copy handler list because the list is not stable.
-        # event handler may block/switch thread execution
-        # and un/register other handlers.
-        #
         handlers = copy.copy(self.events[ev.__class__])
 
         for h in handlers:
             ret = h(ev)
             if ret is False:
                 break
+
+
+class EventQueueBase(event.EventBase):
+    def __init__(self, ev_q):
+        super(EventQueueBase, self).__init__()
+        self.ev_q = ev_q
+        self.aux = ev_q.aux
+
+
+class EventQueueCreate(EventQueueBase):
+    def __init__(self, ev_q, create):
+        super(EventQueueCreate, self).__init__(ev_q)
+        self.create = bool(create)    # True:  queue is created
+                                      # False: queue is destroyed
+        self.dispatcher = ev_q.dispatcher
+
+
+class EventDispatcherChange(EventQueueBase):
+    def __init__(self, ev_q, old_dispatcher, new_dispatcher):
+        super(EventDispatcherChange, self).__init__(ev_q)
+        self.old_dispatcher = old_dispatcher
+        self.new_dispatcher = new_dispatcher
+
+
+DISPATCHER_NAME_QUEUE_EV = 'queue_event'
+QUEUE_EV_DISPATCHER = EventDispatcher(DISPATCHER_NAME_QUEUE_EV)
+QUEUE_NAME_QEV_Q = 'queue_event'
+QUEUE_EV_Q = EventQueue(QUEUE_NAME_QEV_Q, QUEUE_EV_DISPATCHER)
+EventQueue.ev_q_weakref = weakref.ref(QUEUE_EV_Q)
