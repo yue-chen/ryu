9866ef182cf440f437078a3c6d5586907101e54a
Isaku Yamahata <yamahata@valinux.co.jp>
2012-01-20 17:47:48 +0900

    app/event_dumper: visit all possible event queue/dispatcher
    Now it becomes more reliable and simplify.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/app/event_dumper.py
+++ ryu/app/event_dumper.py
@@ -16,21 +16,56 @@
 import gflags
 import logging
 
-from ryu import utils
+from ryu.controller import dispatcher
+from ryu.controller.handler import set_ev_cls
 
 LOG = logging.getLogger('ryu.app.event_dumper')
 
 FLAGS = gflags.FLAGS
-gflags.DEFINE_multistring('dump_dispatcher',
-                          ['ryu.controller.handler.MAIN_DISPATCHER'],
-                          'list of dispatchers to dump event')
+gflags.DEFINE_multistring('dump_queue', [],
+                          'list of dispatcher name to dump event: default any')
+gflags.DEFINE_multistring('dump_dispatcher', [],
+                          'list of dispatcher name to dump event: default any')
 
 
 class EventDumper(object):
     def __init__(self, *_args, **_kwargs):
-        for d in FLAGS.dump_dispatcher:
-            d = utils.import_object(d)
-            d.register_all_handler(self._dump_event)
+        # EventDispatcher can be created and cloned before us.
+        # So register it explicitly
+        for ev_q in dispatcher.EventQueue.event_queues:
+            if ev_q == dispatcher.QUEUE_EV_Q:
+                continue
+            LOG.info('%s: registering q %s dispatcher %s',
+                     __name__, ev_q.name, ev_q.dispatcher.name)
+            self._register_dump_handler(ev_q, ev_q.dispatcher)
+
+    @staticmethod
+    def _need_dump(name, name_list):
+        return len(name_list) == 0 or name in name_list
+
+    def _register_dump_handler(self, ev_q, dispatcher):
+        if (self._need_dump(ev_q.name, FLAGS.dump_queue) or
+            self._need_dump(dispatcher.name, FLAGS.dump_dispatcher)):
+            dispatcher.register_all_handler(self._dump_event)
+
+    @set_ev_cls(dispatcher.EventQueueCreate, dispatcher.QUEUE_EV_DISPATCHER)
+    def queue_create(self, ev):
+        if ev.create:
+            LOG.info('%s: queue created %s', __name__, ev.ev_q.name)
+        else:
+            LOG.info('%s: queue deleted %s', __name__, ev.ev_q.name)
+
+        self._dump_event(ev)
+        self._register_dump_handler(ev.ev_q, ev.dispatcher)
+
+    @set_ev_cls(dispatcher.EventDispatcherChange,
+                dispatcher.QUEUE_EV_DISPATCHER)
+    def dispatcher_change(self, ev):
+        LOG.info('%s: dispatcher change q %s dispatcher %s -> %s', __name__,
+                 ev.ev_q.name, ev.old_dispatcher.name, ev.new_dispatcher.name)
+
+        self._dump_event(ev)
+        self._register_dump_handler(ev.ev_q, ev.new_dispatcher)
 
     def _dump_event(self, ev):
-        LOG.info('event %s' % ev)
+        LOG.info('%s: event %s', __name__, ev)
